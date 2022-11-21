a2fe6dbd114d63dd49906aae5e7f6470c17e02d6
Halil Toma <tomahalil1@gmail.com>
2022-11-21 14:58:22 +0300

    recipe search bar edit

--- src/Routes/View/client/Recipe/Recipe.js
+++ src/Routes/View/client/Recipe/Recipe.js
@@ -5,31 +5,50 @@ import RecipeItems from './RecipeItems';
 
 
 const Recipe = () => {
- 
+  const [searchClass,setSearchClass ]= useState('recipe-content-show d-none bg-light')
+  const [data,setData]=useState([
+    {
+    title:'recipe1',
+    content:'reccipe content ne yazarsam o'
+  },
+    {title:'recipe2',
+    content:'asdasdasd coadsasdentasde asydasdas o'
+  },
+]);
+const [inputData,setInputData]=useState([])
+
+const handleChange=(event)=>{
+  
+const input = data.filter((item)=>{
+  return item.title.includes(event.target.value)
+})
+setInputData(input)
+if(event.target.value !== ''){
+  setSearchClass('recipe-content-show bg-light d-block')
+}else{
+  setSearchClass('recipe-content-show d-none')
+}
+if(input.length == 0){
+  setSearchClass('recipe-content-show d-none')
+}
+}
   return (
     <>
-        <section
-        className="forum-search"
-        style={{ backgroundImage: `url(${bgforum})` }}
-      >
-        <div className="search-bar container d-flex justify-content-center align-items-center">
-          <form
-            action=""
-            className="d-flex align-items-center p-3 bg-dark rounded rounded-pill"
-          >
-            <div className="countries mx-3">
-              <input
-                type="text"
-                placeholder="Recipes"
-                className="form-control"
-              />
-                </div>
-            <button type="submmit" className="btn btn-outline-success mx-3">
-              Search
-            </button>
-          </form>
-        </div>
-      </section>
+    <section className='recipe-home d-flex justify-content-center align-items-center' style={{backgroundImage:`url(${bgforum})`}}>
+      <div  className="recipe-search-bar position-relative border-4 hover border border-danger d-flex gap-4 p-3 bg-light rounded-pill">
+          <input className='form-control' placeholder='Recipe name enter' type="text"  onChange={handleChange} />
+          <button className='btn btn-danger'>Search</button>
+          <div className={searchClass} >
+          <ul className='px-4 m-0 d-flex flex-column'>
+              { inputData!==''  &&
+                inputData.map((item,index)=>(
+                  <li className='p-2 border shadow btn border-1 mx-4 my-2' key={index}><Link to={item.content}>{item.title}</Link></li>
+                ))
+              }
+              </ul>
+          </div>
+      </div>
+    </section>
       
       
     </>
--- src/components/404/Error.js
+++ src/components/404/Error.js
@@ -1,8 +1,13 @@
 import React from 'react'
-
 const Error = () => {
   return (
-    <div>404</div>
+    <div className='error-page d-flex justify-content-center align-items-center'>
+        <div className="content text-center">
+        <h1 className='shadow p-3 rounded text-bg-light'>Schefing </h1>
+        <h2>Oops Not Page</h2>
+              <h3 className='text-danger border border-danger rounded p-2'>404 Error Page</h3>
+        </div>
+    </div>
   )
 }
 
